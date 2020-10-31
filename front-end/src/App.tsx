4da5f78e182afdda00459925eb776aa2ebb3d56b
Bernardo Generoso <58465456+bernardogeneroso@users.noreply.github.com>
2020-10-31 16:38:14 +0000

    Building the project structure

---
+++ front-end/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import GlobalStyle from "./styles/global";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>Let's build a Whatsapp front-end</h1>
+      </div>
+
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default App;
